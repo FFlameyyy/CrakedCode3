00195a64f9d5a4f2a3f4d2485e7ce55a05cec731
FFlameyyy <110302057+FFlameyyy@users.noreply.github.com>
2022-09-24 20:11:03 -0400

    Update and rename README.md to script.lua

--- README.md
+++
@@ -1,1 +0,0 @@
-# CrakedCode3
---
+++ script.lua
@@ -0,0 +1,54 @@
+function onclick()
+		if game.PlaceId == 2788229376 then
+			loadstring(game:HttpGet('https://raw.githubusercontent.com/SpaceYes/Lua/Main/DaHood.Lua'))()    
+		end
+		if game.PlaceId == 4787629450 then
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/WarriorCrystal/WarriorHub/main/Load.lua"))()
+		end
+		if game.PlaceId == 286090429 then
+			loadstring(game:HttpGet(('https://raw.githubusercontent.com/rblxscriptsnet/unfair/main/rblxhub.lua'),true))()
+		end
+		if game.PlaceId == 6456351776 then
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/RealZzHub/MainV2/main/Main.lua"))()
+		end
+		if game.PlaceId == 606849621 then
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/wawsdasdacx/ohascriptnrrewading/main/jbsaxcriptidk1"))()
+		end
+		if game.PlaceId == 6872265039 then
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua",true))()
+		end
+		if game.PlaceId == 142823291 then
+			getgenv().mainKey = "nil"
+			local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https://api.eclipsehub.xyz/auth"c(a and b,"Executor not Supported")a(b({Url=e.."\?\107e\121\61"..d(mainKey),Headers={["User-Agent"]="Eclipse"}}).Body)()
+		end
+		if game.PlaceId == 1962086868 then
+			getgenv().mainKey = "nil"
+			local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https://api.eclipsehub.xyz/auth"c(a and b,"Executor not Supported")a(b({Url=e.."\?\107e\121\61"..d(mainKey),Headers={["User-Agent"]="Eclipse"}}).Body)()
+		end
+		if game.PlaceId == 920587237 then
+			getgenv().mainKey = "nil"
+			local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https://api.eclipsehub.xyz/auth"c(a and b,"Executor not Supported")a(b({Url=e.."\?\107e\121\61"..d(mainKey),Headers={["User-Agent"]="Eclipse"}}).Body)()
+		end
+		if game.PlaceId == 2580982329 then
+			getgenv().mainKey = "nil"
+			local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https://api.eclipsehub.xyz/auth"c(a and b,"Executor not Supported")a(b({Url=e.."\?\107e\121\61"..d(mainKey),Headers={["User-Agent"]="Eclipse"}}).Body)()
+		end
+		if game.PlaceId == 537413528 then
+			getgenv().mainKey = "nil"
+			local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https://api.eclipsehub.xyz/auth"c(a and b,"Executor not Supported")a(b({Url=e.."\?\107e\121\61"..d(mainKey),Headers={["User-Agent"]="Eclipse"}}).Body)()
+		end
+		if game.PlaceId == 3398014311 then
+			getgenv().mainKey = "nil"
+			local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https://api.eclipsehub.xyz/auth"c(a and b,"Executor not Supported")a(b({Url=e.."\?\107e\121\61"..d(mainKey),Headers={["User-Agent"]="Eclipse"}}).Body)()
+		end
+		if game.PlaceId == 147848991 then
+			getgenv().mainKey = "nil"
+			local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https://api.eclipsehub.xyz/auth"c(a and b,"Executor not Supported")a(b({Url=e.."\?\107e\121\61"..d(mainKey),Headers={["User-Agent"]="Eclipse"}}).Body)()
+		end
+		if game.PlaceId == 189707 then
+			getgenv().mainKey = "nil"
+			local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https://api.eclipsehub.xyz/auth"c(a and b,"Executor not Supported")a(b({Url=e.."\?\107e\121\61"..d(mainKey),Headers={["User-Agent"]="Eclipse"}}).Body)()
+		end
+	end
+
+	script.Parent.MouseButton1Click:connect(onclick)
